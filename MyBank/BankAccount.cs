b760b2b3defd1835342eb1d8e7b72be30f4641d7
abdmun8 <abdmun8@gmail.com>
2020-07-30 14:12:49 +0700

    Initial Commit

---
+++ MyBank/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        private static int accountNumberSeed = 123456;
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 0);
+            Console.WriteLine($"{account.Owner} has Account {account.Number} with {account.Balance} balance" );
+        }
+    }
+}
